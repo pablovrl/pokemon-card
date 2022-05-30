bd845a95cc4e798ce1ea4badf05c19ba9cd9c952
Pablo Villarroel <pablovillarroel135@gmail.com>
2022-05-30 02:05:26 -0400

    update Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,38 @@
+import { ImLoop } from "react-icons/im";
+import { AiFillStar } from "react-icons/ai";
+import React from "react";
+
 interface NavbarProps {
   getNewPokemon: () => void;
   openModal: () => void;
 }
 
+const NavbarButton = ({
+  onClick,
+  children,
+}: {
+  onClick: () => void;
+  children: React.ReactNode;
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      className="bg-white p-4 my-2 rounded-full shadow-2xl sm:transform sm:transition sm:duration-300 sm:hover:scale-125"
+    >
+      {children}
+    </button>
+  );
+};
+
 const Navbar = ({ getNewPokemon, openModal }: NavbarProps) => {
   return (
-    <nav className="grid grid-cols-2 box sm:fixed bg-white w-full h-14 sm:h-10 items-center justify-center z-50">
-      <button className="h-full bg-green-600" onClick={getNewPokemon}>
-        New pokemon
-      </button>
-      <button className="h-full bg-yellow-200" onClick={openModal}>
-        Favorites
-      </button>
+    <nav className="absolute top-0 left-0 sm:left-5 sm:top-5 flex flex-col rounded-3xl p-2">
+      <NavbarButton onClick={getNewPokemon}>
+        <ImLoop />
+      </NavbarButton>
+      <NavbarButton onClick={openModal}>
+        <AiFillStar />
+      </NavbarButton>
     </nav>
   );
 };
